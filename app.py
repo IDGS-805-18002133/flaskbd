3160ed6755d4ecfb845faf3302d6d88b2cedd01a
IDGS-805-18002133 <18002133@alumnos.utleon.edu.mx>
2025-03-04 18:55:06 -0600

    agregar

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfig
 from models import Alumno
 from models import db
+import forms
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
@@ -15,7 +16,43 @@ def page_not_found(e):
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+    create_form=forms.UserForm2(request.form)
+    alumno=Alumno.query.all() # select * from alumnos
+    return render_template("index.html",form=create_form,alumnos=alumno)
+
+@app.route("/detalles",methods=['GET','POST'])
+def detalles():
+    create_form=forms.UserForm2(request.form)
+    if request.method == 'GET':
+        id=request.args.get('id')
+        alumno=db.session.query(Alumno).filter(Alumno.id==id).first()
+        nom=alumno.nombre
+        ape=alumno.apaterno
+        email=alumno.email
+    return render_template("detalles.html",form=create_form,nom=nom,ape=ape,email=email)
+
+@app.route("/eliminar",methods=['GET','POST'])
+def eliminar():
+    create_form=forms.UserForm2(request.form)
+    if request.method == 'GET':
+        id=request.args.get('id')
+        alumno=db.session.query(Alumno).filter(Alumno.id==id).first()
+        nom=alumno.nombre
+        ape=alumno.apaterno
+        email=alumno.email
+    return render_template("eliminar.html",form=create_form,nom=nom,ape=ape,email=email)
+
+@app.route("/nuevoAlumno",methods=['GET','POST'])
+def nuevoAlumno():
+    create_form=forms.UserForm2(request.form)
+    if request.method == 'POST':
+        alumno=Alumno(nombre=request.form.nombre.data,
+        apaterno=request.form.apaterno.data,
+        email=request.form.email.data)
+        db.session.add(alumno)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template("nuevoAlumno.html",form=create_form)
 
 if __name__ == '__main__':
     csrf.init_app(app)
